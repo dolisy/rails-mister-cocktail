ba3c6f11edf4b3e86415a0b395d50285adb00df6
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-08-18 10:21:36 +0200

    schema done

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  validates :description, presence: true
+
+  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
+  validates :ingredient, presence: true
+
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
